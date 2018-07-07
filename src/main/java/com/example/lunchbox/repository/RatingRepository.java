21ce11dd238c48f844c31b460db778f7f3aa6ef4
Talha Zafar <talhazafar521@gmail.com>
2018-07-07 16:59:01 +0500

    image work done

---
+++ src/main/java/com/example/lunchbox/repository/RatingRepository.java
@@ -0,0 +1,17 @@
+package com.example.lunchbox.repository;
+
+import com.example.lunchbox.model.entity.Ratings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Ratings,Integer> {
+
+    @Query(value = "select avg(stars) as avergae from lunchbox.ratings where foodmaker_id = ?1 ",nativeQuery = true)
+    Double getAverage(Integer foodmakerId);
+
+    List<Ratings> getRatingsByFoodmakerId(Integer foodmakerId);
+}
